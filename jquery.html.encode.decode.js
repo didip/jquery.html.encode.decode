772ff7af5ec47adfbfe9d8cbcdcbb88dc56ec177
didip <didipk@gmail.com>
2009-09-26 12:14:20 -0700

    first commit

---
+++ jquery.html.encode.decode.js
@@ -0,0 +1,14 @@
+jQuery.extend( {
+	isHtmlEncoded: function(str) { 
+		if(str.search(/&amp;/g) != -1 || str.search(/&lt;/g) != -1 || str.search(/&gt;/g) != -1) 
+			return true; 
+		else 
+			return false; 
+	},
+
+	htmlDecode: function(str) {
+   if($.isHtmlEncoded(str))
+     return str.replace(/&amp;/g, '&').replace(/&lt;/g, '<').replace(/&gt;/g, '>'); 
+   return str; 
+	}
+});
